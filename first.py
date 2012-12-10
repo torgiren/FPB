86922323bcab378f869fe7662c602c258326e2bb
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-10 17:12:22 +0100

    Rysowanie kostki poprzez klasę kostki

--- first.py
+++ first.py
@@ -4,24 +4,73 @@ from OpenGL.GLU import *
 import pygame
 from pygame.locals import *
 
-def render():
-	glClear(GL_COLOR_BUFFER_BIT)
-	glBegin(GL_TRIANGLES)
-	glVertex3f(-1.0,0.0,-10)
-	glVertex3f(1.0,0.0,-10)
-	glVertex3f(0.0,1.0,-10)
-	glEnd()
-	pygame.display.flip()
+class obj:
+	def __init__(self,x=0,y=0,z=0):
+		self.__x=x
+		self.__y=y
+		self.__z=z
+class Block(obj):
+	def __init__(self,x=0,y=0,z=0):
+		obj.__init__(self,x,y)
+		self.points=[]
+		self.type=GL_TRIANGLES
+		self.points.append((x+0.0,y+0.0,z+0.0))
+		self.points.append((x+1.0,y+0.0,z+0.0))
+		self.points.append((x+1.0,y+1.0,z+0.0))
+
+		self.points.append((x+0.0,y+0.0,z+0.0))
+		self.points.append((x+1.0,y+1.0,z+0.0))
+		self.points.append((x+0.0,y+1.0,z+0.0))
+
+		self.points.append((x+0.0,y+0.0,z+0.0))
+		self.points.append((x+0.0,y+1.0,z-1.0))
+		self.points.append((x+0.0,y+0.0,z-1.0))
+
+		self.points.append((x+0.0,y+0.0,z-0.0))
+		self.points.append((x+0.0,y+1.0,z-0.0))
+		self.points.append((x+0.0,y+1.0,z-1.0))
+
+		self.points.append((x+1.0,y+0.0,z+0.0))
+		self.points.append((x+1.0,y+1.0,z-1.0))
+		self.points.append((x+1.0,y+0.0,z-1.0))
+
+		self.points.append((x+1.0,y+0.0,z-0.0))
+		self.points.append((x+1.0,y+1.0,z-0.0))
+		self.points.append((x+1.0,y+1.0,z-1.0))
+
+		self.points.append((x+0.0,y+0.0,z-1.0))
+		self.points.append((x+1.0,y+0.0,z-1.0))
+		self.points.append((x+1.0,y+1.0,z-1.0))
+
+		self.points.append((x+0.0,y+0.0,z-1.0))
+		self.points.append((x+1.0,y+1.0,z-1.0))
+		self.points.append((x+0.0,y+1.0,z-1.0))
+
+
+		#self.points.append((x+0.0,y+0.0,z+0.0))
+		#self.points.append((x+0.0,y+0.0,z+0.0))
+		#self.points.append((x+0.0,y+0.0,z+0.0))
+
+	def RetType(self):
+		return self.type
+	def RetPoints(self):
+		return self.points
+class GraphEngine:
+	def __init__(self):
+		pass	
+	def draw(self,obj):
+		glBegin(obj.RetType())
+		points=obj.RetPoints()
+		for p in points:
+			glVertex3f(*p)
+		glEnd()
+			
+
 pygame.init()
 pygame.display.set_mode((800,600),HWSURFACE|OPENGL|DOUBLEBUF)
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
 glViewport(0,0,800,600)
-
-
-#glutInit()
-#glutInitWindowSize(800,600)
-#glutCreateWindow("First")
-#glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-#glutDisplayFunc(render)
 glClearColor(1.0,1.0,1.0,0.0)
 glColor3f(0.0,0.0,0.0)
 glPointSize(2.0)
@@ -30,8 +79,10 @@ glLoadIdentity()
 gluPerspective( 60.0, float( 800 ) / float( 600 ), 0.1, 1000.0 )
 glMatrixMode(GL_MODELVIEW)
 glLoadIdentity()
+graph=GraphEngine()
+block=Block(z=-5,y=-2,x=-2)
 while True:
-	render()
 	pygame.time.delay(100)
-#gluOrtho2D(0.0,800.0,0.0,600.0)
-#glutMainLoop()
+	glClear(GL_COLOR_BUFFER_BIT)
+	graph.draw(block)
+	pygame.display.flip()
