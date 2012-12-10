547030bba8339f0a208317a57ca6d1a1e5b3a473
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2012-12-10 16:24:47 +0100

    Trojkat na pygame

--- first.py
+++ first.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
+import pygame
+from pygame.locals import *
 
 def render():
 	glClear(GL_COLOR_BUFFER_BIT)
 	glBegin(GL_TRIANGLES)
-	glVertex2i(100,200)
-	glVertex2i(200,200)
-	glVertex2i(150,300)
+	glVertex3f(-1.0,0.0,-10)
+	glVertex3f(1.0,0.0,-10)
+	glVertex3f(0.0,1.0,-10)
 	glEnd()
-	glFlush()
-glutInit()
-glutInitWindowSize(800,600)
-glutCreateWindow("First")
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-glutDisplayFunc(render)
+	pygame.display.flip()
+pygame.init()
+pygame.display.set_mode((800,600),HWSURFACE|OPENGL|DOUBLEBUF)
+glViewport(0,0,800,600)
+
+
+#glutInit()
+#glutInitWindowSize(800,600)
+#glutCreateWindow("First")
+#glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+#glutDisplayFunc(render)
 glClearColor(1.0,1.0,1.0,0.0)
 glColor3f(0.0,0.0,0.0)
 glPointSize(2.0)
 glMatrixMode(GL_PROJECTION)
 glLoadIdentity()
-gluOrtho2D(0.0,800.0,0.0,600.0)
-glutMainLoop()
+gluPerspective( 60.0, float( 800 ) / float( 600 ), 0.1, 1000.0 )
+glMatrixMode(GL_MODELVIEW)
+glLoadIdentity()
+while True:
+	render()
+	pygame.time.delay(100)
+#gluOrtho2D(0.0,800.0,0.0,600.0)
+#glutMainLoop()
